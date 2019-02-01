b2586340108896e79cf6bef7db0599ae4a4fcc65
Hyukishi <insignificant786@gmail.com>
2019-01-31 21:44:13 -0600

    Update piholeimgmount.sh

--- piholeimgmount.sh
+++ piholeimgmount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Created by Jeffery Grantham 1/8/2019
 # Usage: piholeimgmount.sh [image]
-# This script assumes that you've already made a folder under /mint called loop
+# This script assumes that you've already made a folder under /mnt called loop
 
 image=$1
 
