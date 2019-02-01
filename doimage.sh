878ec854bcf635f1dd79e392d7d523f19ceb8c11
Hyukishi <insignificant786@gmail.com>
2019-01-31 21:06:11 -0600

    Update doimage.sh

--- doimage.sh
+++ doimage.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script is intended for usage in a corn job to periodically create an image of the sd card
+# a long with logging upon successful image creation.
 DATE=`date +%d-%b-%Y`
 
 /bin/dd if=/dev/mmcblk0 iflag=direct of=/media/usb/jgpiholebackup/jgpihole_$DATE.img
