a0e1047c729d49b9afc31b97512bdf53b9693b0a
jgrantham <insignificant786@gmail.com>
2019-08-15 10:36:02 -0500

    Update doimage.sh

--- doimage.sh
+++ doimage.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-# This script is intended for usage in a cron job to periodically create an image of the sd card
-# along with logging upon successful image creation.
 DATE=`date +%d-%b-%Y`
 
-/bin/dd if=/dev/mmcblk0 iflag=direct of=/media/usb/jgpiholebackup/jgpihole_$DATE.img
+/bin/dd bs=4M if=/dev/mmcblk0 iflag=direct of=/media/usb/jgpiholebackup/jgpihole_$DATE.img
 if [ $? != 0 ]; then
 	echo "Image creation failed.	$(date)">> /var/log/doimage.log
 			exit 1
+fi
+
+cd /media/usb/jgpiholebackup/
+/sbin/losetup -P /dev/loop0 jgpihole_$DATE.img && /sbin/e2fsck -f -y /dev/loop0p2 && /sbin/losetup -D
+if [ $? != 0 ]; then
+	echo "Filesystem check/repair failed.	$(date)">> /var/log/doimage.log
+			exit 1
+fi
+
+/media/usb/jgpiholebackup/pishrink.sh -s /media/usb/jgpiholebackup/jgpihole_$DATE.img
+if [ $? != 0 ]; then
+	echo "Image shrink failed.	$(date)"
 fi
 	echo "Backup image created successfully.	$(date)">> /var/log/doimage.log
 	cat /var/log/doimage.log | grep "$DATE"
